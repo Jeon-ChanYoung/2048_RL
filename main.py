b0913c84ee03a1fb3fdb16001862c256f385ef51
Jeon-ChanYoung <162970543+Jeon-ChanYoung@users.noreply.github.com>
2025-09-03 10:32:16 +0000

    port 10000

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def root():
     return FileResponse(os.path.join("static", "index.html"))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
